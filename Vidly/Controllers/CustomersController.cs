384dd36241d3739371f66f18f21705f59156250f
Gilbert Rosario <grosario@oriontek.do>
2019-12-30 16:25:21 -0400

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+
+        [Route("Customers")]
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith"},
+                new Customer { Id = 2, Name = "Will Rogers" }
+            };
+
+            return View(new CustomersViewModel { Customers = customers});
+        }
+
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            return Content("Customer id = " + id);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+
+        [Route("Movies")]
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Name = "Iron Man"},
+                new Movie { Name = "The Dark Knight" }
+            };
+
+            return View(new MoviesViewModel { Movies = movies });
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Iron Man" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
